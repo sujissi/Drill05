8149d6b036afe04cad106f1e4c8e4351fc0da8a8
sujissi_Laptop <hwangsuji329@gmail.com>
2023-09-30 01:26:48 +0900

    랜덤 위치 설정

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -1,3 +1,5 @@
+import random
+
 from pico2d import *
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
@@ -6,6 +8,7 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 arrow = load_image('hand_arrow.png')
+
 
 def handle_events():
     global running
@@ -20,12 +23,20 @@ def handle_events():
 
 
 def set_arrow_point():
+    global rx, ry, is_collision
+    margin = 100
+    if is_collision:
+        rx = random.randint(0 + margin, TUK_WIDTH - margin)
+        ry = random.randint(0 + margin, TUK_HEIGHT - margin)
+        is_collision = False
     pass
 
 
 running = True
+is_collision = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
-rx, ry = x, y;
+rx, ry = x, y
+
 frame = 0
 hide_cursor()
 
@@ -43,7 +54,3 @@ while running:
     handle_events()
 
 close_canvas()
-
-
-
-
